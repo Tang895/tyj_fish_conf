b6247b5d2578ded8c9617640db3c5a356f32626d
ian_xiaoxin_linux <tangyijjj@outlook.com>
2025-11-08 21:17:21 +0800

    add some functions🥳

--- config.fish
+++ config.fish
@@ -2,6 +2,18 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# alias 
+# alias cls="clear"
+alias ff="fastfetch"
+alias python="python3"
 
 set -x PATH $PATH /home/linuxbrew/.linuxbrew/bin/
 
+# functions
+function cdfish
+    cd ~/.config/fish/
+end
+
+function cdkitty
+    cd ~/.config/kitty/
+end
